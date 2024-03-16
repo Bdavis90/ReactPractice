a0d98de6d7cb19ab5fcf7bf3920ac188e89c8722
Brandon Davis <wowzerss90@yahoo.com>
2024-03-15 21:59:02 -0400

    Added data.js file to test reusable component. Passed data into component through props using custon attributes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import reactImg from "./assets/react-core-concepts.png"
+import componentsImg from "./assets/components.png"
+import { CORE_CONCEPTS } from "./data.js";
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header()
 {
+
+  const description = reactDescriptions[genRandomInt(2)]
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+  )
+}
+
+function CoreConcepts({title, description, image})
+{
+  return (
+    <li>
+      <img src={image} alt="..."/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   )
 }
 
@@ -17,7 +40,17 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2 >Core Concepts</h2>
+        <ul>
+          {/* Passing object into component as props */}
+          {CORE_CONCEPTS.map(item => <CoreConcepts {...item}/>)}
+          {/* <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+          <CoreConcepts title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+          <CoreConcepts title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+          <CoreConcepts title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/> */}
+        </ul>
+        </section>
       </main>
     </div>
   );
